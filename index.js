5d31775bdf359a21795cbddf2b3f58bd3ac1560e
Kohinoor <kohinoorkapali@27gmail.com>
2026-01-05 19:52:21 +0545

    Connect register frontend to backend API

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors";
 import { connection } from "./Database/db.js";
 import { userRouter } from "./Routes/userRoutes.js";
 import authRouter from "./Routes/authRoutes.js"; 
 
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173", // frontend URL
+  methods: ["GET", "POST", "PATCH", "DELETE"],
+  credentials: true,
+}));
+
 
 connection();
 
