c97c6dc4f93e3d4c0355e3254d5e686b1199d6c3
Kohinoor <kohinoorkapali@27gmail.com>
2026-01-22 08:56:32 +0545

    image added

--- Database/db.js
+++ Database/db.js
@@ -5,7 +5,7 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(
   "mithometer",    
   "postgres",     
-  "rohini123",    
+  "kohinoor",    
   {
     host: "localhost",
     dialect: "postgres",
